7ee55bf943293f143fd8c39ab3b93b1379957e42
Beasters <1229435236@qq.com>
2020-08-22 22:39:33 +0800

    create math folder & upload

---
+++ 数学/_50_Pow.java
@@ -0,0 +1,49 @@
+package 数学;
+
+/**
+ * https://leetcode-cn.com/problems/powx-n/
+ *
+ * @author beastars
+ */
+public class _50_Pow {
+    /**
+     * 递归
+     * 时间复杂度： O(log n)
+     * 空间复杂度： O(log n)
+     */
+    public double myPow(double x, int n) {
+        if (n == 0)
+            return 1;
+
+        if (n == -1)
+            return 1/x;
+
+        boolean odd = (n & 1) == 1; // 如果与上1为1，说明是奇数
+        double half = myPow(x, n >> 1);
+        half *= half;
+        return odd ? (half * x) : half;
+    }
+
+    /**
+     * 非递归
+     * 快速幂
+     * 时间复杂度： O(log n)
+     * 空间复杂度： O(1)
+     */
+    public double myPow2(double x, int n) {
+        double result = 1.0;
+        // 将n转化成long类型，因为n 是 32 位有符号整数，其数值范围是 [−2^31, 2^31 − 1]，如果是负数，int会越界
+        long y = (n < 0) ? -((long) n) : n;
+        while (y > 0) {
+            // 让二进制与上1
+            if ((y & 1) == 1) { // 如果与上1为1，说明最后一位为1，是有值的，更新结果
+                result = result * x;
+            }
+            x *= x;
+            // 舍弃掉最后一个二进制位
+            y = y >> 1;
+        }
+
+        return (n < 0) ? (1 / result) : result;
+    }
+}
---
+++ 数学/剑指Offer_62_圆圈中最后剩下的数字.java
@@ -0,0 +1,36 @@
+package 数学;
+
+/**
+ * https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/
+ *
+ * @author beastars
+ */
+public class 剑指Offer_62_圆圈中最后剩下的数字 {
+    /**
+     * 递归
+     * 公式： f(n, m) = (f(n-1, m) + m) % n
+     */
+    public int lastRemaining(int n, int m) {
+        if (n == 1)
+            return 0;
+        return (lastRemaining(n - 1, m) + m) % n;
+    }
+
+    /**
+     * 非递归
+     */
+    public int lastRemaining2(int n, int m) {
+        // f(1, 3) = 0
+        // f(2, 3) = (f(1, 3) + 3) % 2
+        // ...
+        // f(7, 3) = (f(6, 3) + 3) % 7
+        // f(8, 3) = (f(7, 3) + 3) % 8
+        // f(9, 3) = (f(8, 3) + 3) % 9
+        // f(10, 3) = (f(9, 3) + 3) % 10
+        int result = 0;
+        for (int i = 2; i <= n; i++) {
+            result = (result + m) % i;
+        }
+        return result;
+    }
+}
