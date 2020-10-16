a172aad85286a50a4a8d83bd9510e7a5f41a6c7d
beastars1 <1229435236@qq.com>
2020-10-16 12:56:55 +0800

    upload

--- 数组/_977_有序数组的平方.java
+++ 数组/_977_有序数组的平方.java
@@ -7,24 +7,24 @@ package 数组;
  */
 public class _977_有序数组的平方 {
     public int[] sortedSquares(int[] A) {
-        if (A.length == 1) {
-            A[0] = A[0] * A[0];
-            return A;
-        }
+        if (A == null || A.length == 0)
+            return new int[0];
+        if (A.length == 1)
+            return new int[]{A[0] * A[0]};
 
         int left = 0;
         int right = A.length - 1;
-        int curr = right;
-        int[] arr = new int[A.length];
+        int cur = right;
+        int[] res = new int[A.length];
 
-        while (left <= right) {
-            if (A[left] < 0 && Math.abs(A[left]) >= Math.abs(A[right])) {
-                arr[curr--] = (int) Math.pow(A[left++], 2);
+        while (cur >= 0) {
+            if (Math.abs(A[right]) >= Math.abs(A[left])) {
+                res[cur--] = A[right] * A[right--];
             } else {
-                arr[curr--] = (int) Math.pow(A[right--], 2);
+                res[cur--] = A[left] * A[left++];
             }
         }
 
-        return arr;
+        return res;
     }
 }
