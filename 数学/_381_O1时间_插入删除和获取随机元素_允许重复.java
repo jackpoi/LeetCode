b15d2a40652f9bc123ddb576458f335f8559a3e0
beastars1 <1229435236@qq.com>
2020-10-31 11:36:31 +0800

    upload

--- 数学/_381_O1时间_插入删除和获取随机元素_允许重复.java
+++ 数学/_381_O1时间_插入删除和获取随机元素_允许重复.java
@@ -14,40 +14,40 @@ import java.util.Set;
  * @author beastars
  */
 public class _381_O1时间_插入删除和获取随机元素_允许重复 {
-    Map<Integer, Set<Integer>> idx;
+    Map<Integer, Set<Integer>> map;
     List<Integer> nums;
 
     /** Initialize your data structure here. */
     public _381_O1时间_插入删除和获取随机元素_允许重复() {
-        idx = new HashMap<Integer, Set<Integer>>();
+        map = new HashMap<Integer, Set<Integer>>();
         nums = new ArrayList<Integer>();
     }
 
     /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
     public boolean insert(int val) {
         nums.add(val);
-        Set<Integer> set = idx.getOrDefault(val, new HashSet<Integer>());
+        Set<Integer> set = map.getOrDefault(val, new HashSet<Integer>());
         set.add(nums.size() - 1);
-        idx.put(val, set);
+        map.put(val, set);
         return set.size() == 1;
     }
 
     /** Removes a value from the collection. Returns true if the collection contained the specified element. */
     public boolean remove(int val) {
-        if (!idx.containsKey(val)) {
+        if (!map.containsKey(val)) {
             return false;
         }
-        Iterator<Integer> it = idx.get(val).iterator();
+        Iterator<Integer> it = map.get(val).iterator();
         int i = it.next();
         int lastNum = nums.get(nums.size() - 1);
         nums.set(i, lastNum);
-        idx.get(val).remove(i);
-        idx.get(lastNum).remove(nums.size() - 1);
+        map.get(val).remove(i);
+        map.get(lastNum).remove(nums.size() - 1);
         if (i < nums.size() - 1) {
-            idx.get(lastNum).add(i);
+            map.get(lastNum).add(i);
         }
-        if (idx.get(val).size() == 0) {
-            idx.remove(val);
+        if (map.get(val).size() == 0) {
+            map.remove(val);
         }
         nums.remove(nums.size() - 1);
         return true;
