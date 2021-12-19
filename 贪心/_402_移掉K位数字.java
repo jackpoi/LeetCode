b2ecb296ae30e35e8d5935f875e4721e27b161db
beastars1 <1229435236@qq.com>
2021-12-19 13:41:00 +0800

    update #402

--- 贪心/_402_移掉K位数字.java
+++ 贪心/_402_移掉K位数字.java
@@ -1,5 +1,7 @@
 package 贪心;
 
+import java.util.LinkedList;
+
 /**
  * https://leetcode-cn.com/problems/remove-k-digits/
  */
@@ -26,5 +28,37 @@ public class _402_移掉K位数字 {
             sb.deleteCharAt(0);
         }
         return sb.toString();
+    }
+
+    /**
+     * 栈
+     */
+    public String removeKdigits2(String num, int k) {
+        if (num.length() == k)
+            return "0";
+        LinkedList<Character> stack = new LinkedList<>();
+        char[] chars = num.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            while (k > 0 && !stack.isEmpty() && stack.peekLast() > chars[i]) {
+                k--;
+                stack.pollLast();
+            }
+            stack.offerLast(chars[i]);
+        }
+        for (int i = 0; i < k; i++) {
+            stack.pollLast();
+        }
+        StringBuilder sb = new StringBuilder();
+        // 删除前面的 0
+        boolean zero = true;
+        while (!stack.isEmpty()) {
+            Character c = stack.pollFirst();
+            if (c == '0' && zero) {
+                continue;
+            }
+            zero = false;
+            sb.append(c);
+        }
+        return sb.length() == 0 ? "0" : sb.toString();
     }
 }
